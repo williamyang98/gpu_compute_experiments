57fc9d8a9e96396693cd83548c64b430cff8b0b1
William Yang <williamyang2017@gmail.com>
2025-03-10 03:40:21 +1100

    Use opengl instead of wgpu so we can use cl_khr_gl_sharing extensions

--- rust_cl/src/window.rs
+++ rust_cl/src/window.rs
@@ -1,89 +1,160 @@
 use log::{info, error, debug};
 use std::sync::Arc;
+use std::num::NonZeroU32;
 use super::{
     gui::AppGui,
     app::UserEvent,
 };
 
-struct WgpuWindow {
-    window: Arc<winit::window::Window>,
-    surface_config: wgpu::SurfaceConfiguration,
-    surface: wgpu::Surface<'static>,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    egui_renderer: egui_wgpu::Renderer,
-    egui_state: egui_winit::State,
+struct WinitWindow {
+    window: winit::window::Window,
+    gl: Arc<glow::Context>,
+    gl_context: glutin::context::PossiblyCurrentContext,
+    gl_display: glutin::display::Display,
+    gl_surface: glutin::surface::Surface<glutin::surface::WindowSurface>,
+    egui_glow: egui_glow::EguiGlow,
     is_redraw_requested: bool,
 }
 
-impl WgpuWindow {
-    async fn new(winit_window: winit::window::Window, context: egui::Context) -> anyhow::Result<Self> {
-        let window = Arc::new(winit_window);
-        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-            backends: wgpu::Backends::from_env().unwrap_or(wgpu::Backends::PRIMARY),
-            backend_options: wgpu::BackendOptions::from_env_or_default(),
-            flags: wgpu::InstanceFlags::from_build_config().with_env(),
-        });
-        let surface = instance.create_surface(window.clone())?;
-        let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: Default::default(),
-                force_fallback_adapter: false,
-                compatible_surface: Some(&surface),
+impl WinitWindow {
+    async fn new(event_loop: &winit::event_loop::ActiveEventLoop) -> anyhow::Result<Self> {
+        // Source: https://github.com/emilk/egui/blob/0.31.1/crates/egui_glow/examples/pure_glow.rs
+        use glutin::context::NotCurrentGlContext;
+        use glutin::display::GetGlDisplay;
+        use glutin::display::GlDisplay;
+        use glutin::prelude::GlSurface;
+        use winit::raw_window_handle::HasWindowHandle;
+
+        let winit_window_builder = winit::window::WindowAttributes::default()
+            .with_resizable(true)
+            .with_inner_size(winit::dpi::LogicalSize {
+                width: 800.0,
+                height: 600.0,
             })
-            .await
-            .ok_or(anyhow::Error::msg("Failed to find valid wgpu adapter"))?;
+            .with_title("egui_glow example") // Keep hidden until we've painted something. See https://github.com/emilk/egui/pull/2279
+            .with_visible(false);
 
-        let (device, queue) = adapter
-            .request_device(&wgpu::DeviceDescriptor::default(), None)
-            .await?;
+        let config_template_builder = glutin::config::ConfigTemplateBuilder::new()
+            .prefer_hardware_accelerated(None)
+            .with_depth_size(0)
+            .with_stencil_size(0)
+            .with_transparency(false);
 
-        let initial_window_size = window.inner_size();
+        log::debug!("trying to get gl_config");
+        let (mut window, gl_config) =
+            glutin_winit::DisplayBuilder::new() // let glutin-winit helper crate handle the complex parts of opengl context creation
+                .with_preference(glutin_winit::ApiPreference::FallbackEgl) // https://github.com/emilk/egui/issues/2520#issuecomment-1367841150
+                .with_window_attributes(Some(winit_window_builder.clone()))
+                .build(
+                    event_loop,
+                    config_template_builder,
+                    |mut config_iterator| {
+                        config_iterator.next().expect(
+                            "failed to find a matching configuration for creating glutin config",
+                        )
+                    },
+                )
+                .expect("failed to create gl_config");
+        let gl_display = gl_config.display();
+        log::debug!("found gl_config: {:?}", &gl_config);
 
-        let mut surface_config = surface
-            .get_default_config(&adapter, initial_window_size.width, initial_window_size.height)
-            .ok_or(anyhow::Error::msg("Failed to get default config for wgpu surface"))?;
-        surface_config.format = wgpu::TextureFormat::Bgra8Unorm;
-        surface_config.present_mode = wgpu::PresentMode::AutoVsync;
-        surface.configure(&device, &surface_config);
+        let raw_window_handle = window.as_ref().map(|w| {
+            w.window_handle()
+                .expect("failed to get window handle")
+                .as_raw()
+        });
+        log::debug!("raw window handle: {:?}", raw_window_handle);
+        let context_attributes =
+            glutin::context::ContextAttributesBuilder::new().build(raw_window_handle);
+        // by default, glutin will try to create a core opengl context. but, if it is not available, try to create a gl-es context using this fallback attributes
+        let fallback_context_attributes = glutin::context::ContextAttributesBuilder::new()
+            .with_context_api(glutin::context::ContextApi::Gles(None))
+            .build(raw_window_handle);
+        let not_current_gl_context = unsafe {
+            gl_display
+                    .create_context(&gl_config, &context_attributes)
+                    .unwrap_or_else(|_| {
+                        log::debug!("failed to create gl_context with attributes: {:?}. retrying with fallback context attributes: {:?}",
+                            &context_attributes,
+                            &fallback_context_attributes);
+                        gl_config
+                            .display()
+                            .create_context(&gl_config, &fallback_context_attributes)
+                            .expect("failed to create context even with fallback attributes")
+                    })
+        };
 
-        let egui_renderer = egui_wgpu::Renderer::new(
-            &device,
-            surface_config.format, None,
-            1,
-            false,
+        // this is where the window is created, if it has not been created while searching for suitable gl_config
+        let window = window.take().unwrap_or_else(|| {
+            log::debug!("window doesn't exist yet. creating one now with finalize_window");
+            glutin_winit::finalize_window(event_loop, winit_window_builder.clone(), &gl_config)
+                .expect("failed to finalize glutin window")
+        });
+        let (width, height): (u32, u32) = window.inner_size().into();
+        let width = NonZeroU32::new(width).unwrap_or(NonZeroU32::MIN);
+        let height = NonZeroU32::new(height).unwrap_or(NonZeroU32::MIN);
+
+        let surface_attributes =
+            glutin::surface::SurfaceAttributesBuilder::<glutin::surface::WindowSurface>::new()
+                .build(
+                    window
+                        .window_handle()
+                        .expect("failed to get window handle")
+                        .as_raw(),
+                    width,
+                    height,
+                );
+        log::debug!(
+            "creating surface with attributes: {:?}",
+            &surface_attributes
         );
+        let gl_surface = unsafe {
+            gl_display
+                .create_window_surface(&gl_config, &surface_attributes)
+                .unwrap()
+        };
+        log::debug!("surface created successfully: {gl_surface:?}.making context current");
+        let gl_context = not_current_gl_context.make_current(&gl_surface).unwrap();
 
-        let viewport_id = context.viewport_id();
-        let native_pixels_per_point = context.native_pixels_per_point();
+        gl_surface
+            .set_swap_interval(
+                &gl_context,
+                glutin::surface::SwapInterval::Wait(NonZeroU32::MIN),
+            )
+            .unwrap();
 
-        let egui_state = egui_winit::State::new(
-            context,
-            viewport_id,
-            &window,
-            native_pixels_per_point,
-            window.theme(),
-            None,
-        );
+        let glow_context = unsafe {
+            glow::Context::from_loader_function(|s| {
+                let s = std::ffi::CString::new(s).expect("failed to construct C string from string for gl proc address");
+                gl_display.get_proc_address(&s)
+            })
+        };
+        let glow_context = Arc::new(glow_context);
+        let egui_glow = egui_glow::EguiGlow::new(event_loop, glow_context.clone(), None, None, true);
+
+        window.set_visible(true);
+
 
         Ok(Self {
             window,
-            surface_config,
-            surface,
-            device,
-            queue,
-            egui_renderer,
-            egui_state,
+            gl: glow_context,
+            gl_context,
+            gl_display,
+            gl_surface,
+            egui_glow,
             is_redraw_requested: false,
         })
     }
 
     fn on_resize(&mut self, width: u32, height: u32) {
-        let width = width.max(100);
-        let height = height.max(100);
-        self.surface_config.width = width;
-        self.surface_config.height = height;
-        self.surface.configure(&self.device, &self.surface_config);
+        let width = NonZeroU32::new(width).unwrap_or(NonZeroU32::MIN);
+        let height = NonZeroU32::new(height).unwrap_or(NonZeroU32::MIN);
+        use glutin::surface::GlSurface;
+        self.gl_surface.resize(
+            &self.gl_context,
+            width,
+            height,
+        );
         self.trigger_redraw();
     }
 
@@ -94,95 +165,51 @@ impl WgpuWindow {
         }
     }
 
-    fn on_redraw_requested(&mut self, render_call: impl FnOnce(&egui::Context)) {
+    fn on_redraw_requested(&mut self, mut render_call: impl FnMut(&egui::Context)) {
         self.is_redraw_requested = false;
+        self.egui_glow.run(&self.window, |context| render_call(context));
 
-        // egui tessellation
-        let raw_input = self.egui_state.take_egui_input(&self.window);
-        let context = self.egui_state.egui_ctx();
-        context.begin_pass(raw_input);
-        render_call(context);
-        let full_output = context.end_pass();
-        let paint_jobs = context.tessellate(full_output.shapes, full_output.pixels_per_point);
-
-        // render pass
-        let frame = self.surface.get_current_texture().expect("Failed to acquire next swap chain texture");
-        let texture_view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let renderer = &mut self.egui_renderer;
-        let window_size = self.window.inner_size();
-        let screen_descriptor = egui_wgpu::ScreenDescriptor {
-            size_in_pixels: [window_size.width, window_size.height],
-            pixels_per_point: full_output.pixels_per_point,
-        };
-
-        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            label: Some("main_window_command_encoder"),
-        });
-
-        // egui render pass
-        for (texture_id, image_delta) in full_output.textures_delta.set.iter() {
-            renderer.update_texture(&self.device, &self.queue, *texture_id, image_delta);
-        }
-        let _command_buffers = renderer.update_buffers(&self.device, &self.queue, &mut encoder, paint_jobs.as_slice(), &screen_descriptor);
-        let rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("egui_render_pass"),
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &texture_view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            occlusion_query_set: None,
-        });
-        let mut rpass = rpass.forget_lifetime();
-        renderer.render(&mut rpass, paint_jobs.as_slice(), &screen_descriptor);
-        // @NOTE: because of the stupid .forget_lifetime() call we need to manually drop this
-        //        otherwise the renderpass will persist after its supposed to be moved into the encoder
-        //        and cause a very confusing error about the renderpass not being finished
-        drop(rpass);
-        self.queue.submit(Some(encoder.finish()));
-        frame.present();
+        use glow::HasContext as _;
+        use glutin::surface::GlSurface;
+        unsafe { self.gl.clear_color(1.0, 1.0, 1.0, 1.0); }
+        self.egui_glow.paint(&mut self.window);
+        self.gl_surface.swap_buffers(&self.gl_context).unwrap();
+        self.window.set_visible(true);
     }
 }
 
-
-pub struct MainWindow {
-    gui: AppGui,
-    window: Option<WgpuWindow>,
+impl Drop for WinitWindow {
+    fn drop(&mut self) {
+        self.egui_glow.destroy();
+    }
 }
 
-impl MainWindow {
+pub struct WinitApplication {
+    gui: AppGui,
+    window: Option<WinitWindow>,
+}
+
+impl WinitApplication {
     pub fn new() -> Self {
         Self {
             gui: AppGui::new(),
             window: None,
         }
     }
-
 }
 
-impl winit::application::ApplicationHandler<UserEvent> for MainWindow {
+impl winit::application::ApplicationHandler<UserEvent> for WinitApplication {
     // Apparently windows can get destroyed/created on a whim for mobile platforms which necessitates this
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let context = egui::Context::default();
-        let viewport_builder = egui::ViewportBuilder::default();
-        context.set_fonts(egui::FontDefinitions::default());
-        context.set_style(egui::Style::default());
-        let winit_window = egui_winit::create_window(&context, event_loop, &viewport_builder).unwrap();
-        let wgpu_window = pollster::block_on(WgpuWindow::new(winit_window, context)).unwrap();
-        debug!("Created wgpu window");
-
+        let wgpu_window = pollster::block_on(WinitWindow::new(event_loop)).unwrap();
+        debug!("Created winit window");
         self.window = Some(wgpu_window);
     }
 
     fn window_event(&mut self, event_loop: &winit::event_loop::ActiveEventLoop, _id: winit::window::WindowId, event: winit::event::WindowEvent) {
         let window = self.window.as_mut().expect("Event loop shouldn't be able to exist without window");
         // let egui consume events first
-        let response = window.egui_state.on_window_event(&window.window, &event);
+        let response = window.egui_glow.on_window_event(&window.window, &event);
         if response.repaint {
             window.trigger_redraw();
         }
@@ -208,5 +235,10 @@ impl winit::application::ApplicationHandler<UserEvent> for MainWindow {
             let window = self.window.as_mut().expect("Event loop shouldn't be able to exist without window");
             window.trigger_redraw();
         }
+    }
+
+    fn exiting(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.window = None;
+        debug!("Destroyed winit window");
     }
 }
