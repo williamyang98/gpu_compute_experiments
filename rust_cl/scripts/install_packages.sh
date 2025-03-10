16b08a4007e8af2e9b343235d9e458bf8dd8723a
William Yang <williamyang2017@gmail.com>
2025-03-10 15:20:46 +1100

    Added script to install relevant packages for opencl support on ubuntu

---
+++ rust_cl/scripts/install_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Required to get -lOpenCL linker flag to work
+sudo apt-get install ocl-icd-opencl-dev
+
+# Install specific opencl icd driver
+# sudo apt-get install intel-opencl-icd
+# sudo apt-get install mesa-opencl-icd
